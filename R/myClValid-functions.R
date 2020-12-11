e27968f00e665e8e81b710aad0709af0aea8536d
Biagio98 <biagio.incardona@outlook.it>
2020-12-11 17:41:38 +0100

    Update myClValid-functions.R

--- R/myClValid-functions.R
+++ R/myClValid-functions.R
@@ -436,10 +436,19 @@ mysilhouette <- function(distance=NULL, clusters, Data=NULL, method="euclidean")
 #####################################################################
 
 connectivity <- function(distance=NULL, clusters, Data=NULL, neighbSize=10, method="euclidean"){
-
+  
+  print("distance")
+  print(distance)
+  print("-----------------")
+  print("clusters")
+  print(clusters)
+  print("_______________________")
+  
+  
   if (is.null(distance) & is.null(Data)) stop("One of 'distance' or 'Data' is required")
   if (is.null(distance)) distance <- as.matrix(dist(Data, method=method))
   if (class(distance)=="dist") distance <- as.matrix(distance)
+  
   nearest <- apply(distance,2,function(x) sort(x,ind=TRUE)$ix[2:(neighbSize+1)])
   nr <- nrow(nearest);nc <- ncol(nearest)
   same <- matrix(clusters,nrow=nr,ncol=nc,byrow=TRUE)!=matrix(clusters[nearest],nrow=nr,ncol=nc)
